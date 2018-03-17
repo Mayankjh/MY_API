c6f8c9d4a47e6eeebcbe5d35f8eda0cb96aae54c
Mayank Jha <mayankjha7722@gmail.com>
2018-03-17 20:33:34 +0530

    getting ui setup

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,13 @@
 
 function setup() {
   createCanvas(400, 400);
-  angleMode(DEGREES);
+  background(51);
+  loadJSON('all', gotData);
+  console.log('running');
+}
+
+function gotData(data){
+  console.log(data);
+  var keys = Object.keys(data);
+  console.log(keys);
 }
