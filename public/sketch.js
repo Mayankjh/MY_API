b2a54c978133304ea457587f0cd8a88d50f8847a
Mayank Jha <mayankjha7722@gmail.com>
2018-03-17 22:22:30 +0530

    minor

--- public/sketch.js
+++ public/sketch.js
@@ -1,13 +1,43 @@
 
 function setup() {
-  createCanvas(400, 400);
-  background(51);
-  loadJSON('all', gotData);
+  createCanvas(300, 200);
+  drawData();
   console.log('running');
+
+  var button = select('#submit');
+  button.mousePressed(submitWord);
+
+  function drawData() {
+      loadJSON('all', gotData);
+  }
+
+  function submitWord(){
+    var word = select('#word').value();
+    var score = select('#score').value();
+    console.log( word, score);
+
+    loadJSON('add/' + word + "/" + score, finished);
+
+    function finished(data){
+      console.log(data);
+      drawData();
+    }
+  }
+
 }
 
 function gotData(data){
+    background(51);
   console.log(data);
   var keys = Object.keys(data);
+  for (var i; i< keys.length;i++){
+    var word = keys[i];
+    var score = data[word];
+    var x = random(width);
+    var y = random(height);
+    fill(255);
+    textSize(16);
+    text(word,x,y);
+  }
   console.log(keys);
 }
